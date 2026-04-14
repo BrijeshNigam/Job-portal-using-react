c61c2fcc0f518c7f58933a0871cdc860ff3b85c6
BrijeshNigam <nigambrjesh540@gmail.com>
2026-04-15 02:09:13 +0530

    good night friends i am thak chuka ab need ke jhke aa rhe hai

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,3 +12,30 @@ import {
   ArrowRight,
   AlertCircle,
 } from "lucide-react";
+
+
+
+const Login = () => {
+  const [isRegister, setIsRegister] = useState(false);
+  const [formData, setFormData] = useState({ name: '', email: '', password: '' });
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const { Login } = useAuth();
+  const navigate = useNavigate;
+
+  const handleSubmit = sync {e} => {
+    e.preventDefailt();
+    setError ('');
+    setLoading (true);
+  }
+
+  const endpoint = isRegister ? 'register' : 'login';
+  try {
+    const responce = await fetch(`http://localhost.5000/api/auth`$(endpoint) , { 
+        method: 'POST'; 
+        headers: {'Content-type':'application/json'},
+        baby: JOSN.stringify(FormData),
+    });
+    
+  }
