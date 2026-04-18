1c2ccae8a9ccb940a3b6c6ecc1f759696123609d
BrijeshNigam <nigambrjesh540@gmail.com>
2026-04-18 19:35:13 +0530

    working on prepHub.jsx

---
+++ src/pages/PrepHub.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const PrepHub = () => {
+  const categories = [
+    {
+      title: "Aptitude Training",
+      desc: "Quanttitive, Logical, and Verbal reasoning modules.",
+      icon: <Brain size={30} color="var(--primary)" />,
+      path: "/prep/aptitude",
+      state: "110+ Topics",
+    },
+    {
+      title: "Technical Core",
+      decs: "MAster OS, DBMS, Computer Networks, and OOPs",
+      icon: <Code2 size={32} color="var(--secondary)" />,
+      path: "/prep/technical",
+      stats: "450+ MCQs",
+    },
+    {},
+  ];
+};
